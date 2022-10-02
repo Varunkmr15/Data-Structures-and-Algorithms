e38c5daf3d9f5806145319aba35f361a80af6a1a
Varunkmr15 <97528402+Varunkmr15@users.noreply.github.com>
2022-10-02 19:46:35 +0530

    Create Sieve.cpp

---
+++ Number Theory/Sieve.cpp
@@ -0,0 +1,26 @@
+void sieve(int n)
+{
+	if(n <= 1)
+		return;
+
+	bool isPrime[n+1];
+
+	fill(isPrime, isPrime + n + 1, true);
+
+	for(int i=2; i*i <= n; i++)
+	{
+		if(isPrime[i])
+		{
+			for(int j = i*i; j <= n; j = j+i)
+			{
+				isPrime[j] = false;
+			}
+		}
+	}
+
+	for(int i = 2; i<=n; i++)
+	{
+		if(isPrime[i])
+			cout<<i<<" ";
+	}
+}
