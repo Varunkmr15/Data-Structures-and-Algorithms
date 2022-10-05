e94a17e608741471ed4e6273375da81ae6f5fef1
Varunkmr15 <97528402+Varunkmr15@users.noreply.github.com>
2022-10-05 19:20:17 +0530

    Create LookupTableSetBits.cpp

---
+++ Number Theory/LookupTableSetBits.cpp
@@ -0,0 +1,20 @@
+int table[256];
+	
+	// recursive function to count set bits 
+	void initialize() { 
+
+      // To initially generate the table algorithmically:
+      table[0] = 0;
+      for (int i = 0; i < 256; i++)
+      {
+          table[i] = (i & 1) + table[i / 2];
+      }
+	} 
+	
+	int countSetBits(int n)
+	{
+	    return table[n & 0xff] + 
+        table[(n >> 8) & 0xff] + 
+        table[(n >> 16) & 0xff] + 
+        table[n >> 24]; 
+	}
