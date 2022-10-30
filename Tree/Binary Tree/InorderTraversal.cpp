25f3d309891d73573ae1ee110ab5ac3dd8ebbac0
Varunkmr15 <97528402+Varunkmr15@users.noreply.github.com>
2022-10-30 15:12:41 +0530

    Create InorderTraversal.cpp

---
+++ Tree/Binary Tree/InorderTraversal.cpp
@@ -0,0 +1,18 @@
+struct Node  
+{ 
+  int key; 
+  struct Node *left; 
+  struct Node *right; 
+  Node(int k){
+      key=k;
+      left=right=NULL;
+  }
+};
+
+void inorder(Node *root){
+    if(root!=NULL){
+        inorder(root->left);
+        cout<<root->key<<" ";
+        inorder(root->right);    
+    }
+}  
