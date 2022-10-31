f735253b18efd39c0c8ddd32647256b8311478c3
Varunkmr15 <97528402+Varunkmr15@users.noreply.github.com>
2022-10-31 14:11:27 +0530

    Create BFS.cpp

---
+++ Tree/Binary Tree/BFS.cpp
@@ -0,0 +1,17 @@
+// TC:O(N)
+// SC:O(N)
+
+void printLevel(Node *root){
+    if(root==NULL)return;
+    queue<Node *>q;
+    q.push(root);
+    while(q.empty()==false){
+        Node *curr=q.front();
+        q.pop();
+        cout<<curr->key<<" ";
+        if(curr->left!=NULL)
+            q.push(curr->left);
+        if(curr->right!=NULL)
+            q.push(curr->right);
+    }
+}  
