1ffba9afa449a32be0910cc66d61c9c730928a63
Varunkmr15 <97528402+Varunkmr15@users.noreply.github.com>
2022-11-22 14:56:46 +0530

    Create BinaryHeap.cpp

---
+++ Heap/BinaryHeap.cpp
@@ -0,0 +1,17 @@
+class MinHeap{
+    int *arr;
+    int size;
+    int capacity;
+    
+    public:
+    
+    MinHeap(int c){
+    size = 0; 
+    capacity = c; 
+    arr = new int[c];
+    }
+
+    int left(int i) { return (2*i + 1); } 
+    int right(int i) { return (2*i + 2); } 
+    int parent(int i) { return (i-1)/2; } 
+};
