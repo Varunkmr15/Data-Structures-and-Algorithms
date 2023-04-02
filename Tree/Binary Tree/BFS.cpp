f76af71f43f15a50f8c319ddf65da0e2af71456b
Varunkmr15 <97528402+Varunkmr15@users.noreply.github.com>
2023-04-03 01:41:39 +0530

    Update BFS.cpp

--- Tree/Binary Tree/BFS.cpp
+++ Tree/Binary Tree/BFS.cpp
@@ -15,3 +15,21 @@ void printLevel(Node *root){
             q.push(curr->right);
     }
 }  
+
+vector<int> levelOrder(TreeNode* root) {
+        vector<int> ans; 
+        if(root == NULL) 
+            return ans;  
+        queue<TreeNode*> q; 
+        q.push(root); 
+        while(!q.empty()) {
+            TreeNode *temp = q.front(); 
+            q.pop(); 
+            if(temp->left != NULL) 
+                q.push(temp->left); 
+            if(temp->right != NULL) 
+                q.push(temp->right); 
+            ans.push_back(temp->val); 
+        }
+        return ans; 
+    }
