a7cb8da714055cd38d9abc77e7d14d33140f1314
Varunkmr15 <97528402+Varunkmr15@users.noreply.github.com>
2023-06-04 00:33:02 +0530

    Update BalancedBT.cpp

--- Tree/Binary Tree/BalancedBT.cpp
+++ Tree/Binary Tree/BalancedBT.cpp
@@ -1,17 +1,20 @@
 // tc: O(N)
-// sc: O(H)
+// sc: O(N)
 
 
-int isBalanced(Node *root){
-    if(root==NULL)
-        return 0;
-    int lh=isBalanced(root->left);
-    if(lh==-1)return -1;
-    int rh=isBalanced(root->right);
-    if(rh==-1)return -1;
-    
-    if(abs(lh-rh)>1)
-        return -1; 
-    else
+int height(TreeNode* root){
+        if(root==NULL)return 0;
+
+        int lh=height(root->left);
+        int rh=height(root->right);
+
+        if((abs(lh-rh)>1) || (lh==-1 || rh==-1))return -1;
+
         return max(lh,rh)+1;
-}
+    }
+
+    bool isBalanced(TreeNode* root) {
+        if(height(root)==-1)return 0;
+        else return 1;
+    }
+
